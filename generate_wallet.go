23a5fa52de6aee13126714564c582e8cdead01ae
TranBaVinhSon <tran.ba.vinh.son@mf-financial.jp>
2019-02-03 18:02:51 +0900

    add

---
+++ generate_wallet.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"log"
+)
+
+func main() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+
+	fmt.Printf(hexutil.Encode(privateKeyBytes[2:]))
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Printf(hexutil.Encode(publicKeyBytes[4:]))
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+}
